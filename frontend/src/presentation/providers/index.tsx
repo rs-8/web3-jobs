610e87c3ab326b977f1591d536ca612add76a197
RUSTEM SULTANOV <sultanov.rustem.azatovich@gmail.com>
2022-05-07 19:45:16 +0300

    feat(header): Implement header

--- frontend/src/presentation/providers/index.tsx
+++ frontend/src/presentation/providers/index.tsx
@@ -2,17 +2,25 @@ import React from 'react'
 import {CssBaseline, GeistProvider} from '@geist-ui/react'
 import {ApolloClient, ApolloProvider, InMemoryCache} from '@apollo/client'
 import {AppThemeProvider} from './theme-provider'
+import {createGlobalStyle} from 'styled-components'
 
 const client = new ApolloClient({
   uri: 'http://localhost:3000/graphql',
   cache: new InMemoryCache(),
 })
 
+const GlobalStyle = createGlobalStyle`
+  *, :after, :before, html {
+    box-sizing: border-box;
+  }
+`
+
 const RootProvider: React.FC = ({children}) => {
   return (
     <ApolloProvider client={client}>
       <GeistProvider themeType={'light'}>
         <CssBaseline />
+        <GlobalStyle />
         <AppThemeProvider>{children}</AppThemeProvider>
       </GeistProvider>
     </ApolloProvider>
